d07bc16bae2c206588dbdd2ba4af0cbc72c22559
jgdev101613 <felicisimojv@gmail.com>
2026-05-21 17:14:23 +0800

    changes

--- src/cron.js
+++ src/cron.js
@@ -3,7 +3,7 @@ import http from "node:http";
 import https from "node:https";
 
 // every 14 minutes send a GET request to the health endpoint
-const job = new CronJob("*/1 * * * *", function () {
+const job = new CronJob("*/10 * * * *", function () {
   const base = process.env.BACKEND_URL;
 
   if (!base) {
